be87f99209ac45095494f811cc32feb719ab6948
Harshita <chandaliyaharshita222@gmail.com>
2023-07-15 20:28:21 +0530

    Find max out of 3 integer

---
+++ Generics_Max/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Generics_Max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Problem!");
+            Console.WriteLine("Enter 3 integer numbers one by one");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Find_Max.FindMaxValue(a, b, c);
+        }
+    }
+}
